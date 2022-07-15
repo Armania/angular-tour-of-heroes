01155fb1aa518ddcbd2f509285d7394d0ee77c8c
Armani Appolon <ArmaniAppolon@gmail.com>
2022-07-15 13:05:25 -0400

    Version 1 final

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -32,6 +32,13 @@ export class HeroDetailComponent implements OnInit {
       .subscribe(hero => this.hero = hero);
   }
 
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero)
+        .subscribe(() => this.goBack());
+    }
+  }
+
   goBack(): void {
     this.location.back();
   }
