83443302b3239e83025d86ce43e3c937a0cab2f4
Armani Appolon <ArmaniAppolon@gmail.com>
2022-07-15 17:36:01 -0400

    adding new hero form. part 2 of tutorial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroFormComponent } from './hero-form/hero-form.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   {path: 'dashboard', component: DashboardComponent },
   { path: 'heroes', component: HeroesComponent },
   { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'form', component:HeroFormComponent}
 ];
 
 @NgModule({
--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,4 +1,15 @@
 export interface Hero {
   id: number;
   name: string;
+  power?: string
+  alterEgo?: string
+}
+
+export class Hero {
+  constructor(
+    public id: number,
+    public name: string,
+    public power?: string,
+    public alterEgo?: string
+  ) {  }
 }
